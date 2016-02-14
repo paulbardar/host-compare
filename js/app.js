e764c810325aac3f7319b03656d2d49937679e8a
paulbardar <q.c.tulip@gmail.com>
2016-02-15 01:08:54 +0200

    All without footer

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
-$(document).foundation();
+;(function($){
+	$(function(){
+  		var slider = $('.ba-slider');
+  		slider.slick({
+        gorizontal: true,
+   			dots: false,
+   			infinite:false,
+   			arrow: true
+  		});
+ 	});
+
+ 	$(function(){
+  		var sliderFeatures = $('.ba-slider-features');
+
+  		sliderFeatures.slick({
+   			dots: true,
+   			infinite:false,
+  			arrow:false,
+  			prevArrow: false,
+  			nextArrow:false
+
+  		});
+ 	});
+})(jQuery);
